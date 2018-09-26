6449fd1ccc18520ea60bd2754e990c125ee03771
Kim Keumtae <kimkt1549@gmail.com>
2018-09-26 17:37:09 +0900

    feat : Initial code cleanup

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'styles/base.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
