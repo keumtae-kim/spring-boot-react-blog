ace2b3b2eb2563b149adb9ed64077ec429014f1c
Kim Keumtae <kimkt1549@gmail.com>
2018-10-03 00:36:32 +0900

    refactor : apply navbar state management

--- blog-frontend/src/components/common/Header/Menu/Menu.js
+++ blog-frontend/src/components/common/Header/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Menu.scss';
 import classNames from 'classnames/bind';
@@ -8,38 +8,55 @@ import {
   Navbar,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  NavbarToggler
 } from 'reactstrap';
 
 const cx = classNames.bind(styles);
 
-const Menu = () => (
-  <Navbar className="navbar navbar-expand-lg navbar-light fixed-top is-visible" id="mainNav">
-    <Container>
-      <a className="navbar-brand" href="index.html">Spring React Blog</a>
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-  <i className="fas fa-bars"></i>
-      </button>
-      <Collapse isOpen={true} navbar>
-        <Nav className="navbar-nav ml-auto">
-          <NavItem>
-            <NavLink to="/index">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/about">About</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/post">Sample Post</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/contact">Contact</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Container>
-  </Navbar>
-);
+class Menu extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <Navbar className={cx("navbar", "navbar-expand-lg", "navbar-light", "fixed-top", "is-visible")} id="mainNav">
+        <Container>
+          <a className={cx("navbar-brand")} href="index.html">Spring-Boot React Blog</a>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="navbar-nav ml-auto">
+              <NavItem>
+                <NavLink to="/index">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/about">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/post">Sample Post</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/contact">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
 
 export default Menu;
